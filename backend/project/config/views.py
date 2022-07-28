e3851259c841e651a3f4afc19539bbc7febedf28
Jonghyun Lim <ezemango@gmail.com>
2022-07-28 16:31:31 +0900

    FEAT: '/' 에 hello world api 제작

---
+++ backend/project/config/views.py
@@ -0,0 +1,9 @@
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def hello_world(request):
+    return Response({'message': 'Hello, world!'})
